4d80636f57356dd036da8bfb8217cf0b99d2b82c
n1mah <nima.heidari.n@gmail.com>
2023-06-27 23:52:35 +0330

    install irazasyed/telegram-bot-sdk

--- managerBot/routes/api.php
+++ managerBot/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Telegram\Bot\Laravel\Facades\Telegram;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('bot/sendmessage', function() {
+    Telegram::sendMessage([
+        'chat_id' => 'RECIPIENT_CHAT_ID',
+        'text' => 'Hello world!'
+    ]);
+    return;
+});
+
